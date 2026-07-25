11c94d667340d1fc269d904828e66b5d069e1284
axwinachu <ceaswin146@gmail.com>
2026-07-25 13:41:57 +0530

    updatedapi gateway

--- api-gateway/src/main/java/com/banfico/api_gateway/dto/LoginRequest.java
+++
@@ -1,3 +0,0 @@
-package com.banfico.api_gateway.dto;
-
-public record LoginRequest(String username, String password) {}
--- api-gateway/src/main/java/com/banfico/api_gateway/dto/LoginResponse.java
+++
@@ -1,3 +0,0 @@
-package com.banfico.api_gateway.dto;
-
-public record LoginResponse(String status, long expiresIn) {}
