3e096c43815d51e1dc37beb57681a3d775b166d6
Anzyll <anzil2003m@gmail.com>
2026-07-25 12:07:17 +0530

    updated git ignore

---
+++ extraction-service/src/main/java/com/banfico/extractionservice/ExtractionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banfico.extractionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtractionServiceApplication.class, args);
+    }
+
+}
